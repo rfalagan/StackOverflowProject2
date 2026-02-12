59eb2d4d78db062fb17ea7a118efe865a70e16f6
Rafael Falagan <rfalagan@yahoo.com>
2026-02-12 10:37:35 -0500

    commiting changes

---
+++ StackOverflowProject2/ApiControllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using StackOverflowProject.ServiceLayer;
+using StackOverflowProject.ViewModels;
+
+
+namespace StackOverflowProject2.ApiControllers
+{
+    public class AccountController : ApiController
+    {
+
+        IUsersService us;
+
+        public AccountController(IUsersService us)
+        {
+            this.us = us;
+
+        }
+
+        public string Get(string Email)
+        {
+            // get user name based on email addess
+            if (this.us.GetUsersByEmail(Email) != null)
+            {
+                return "Found";
+            }
+            else
+            {
+                return "Not Found";
+
+            }
+
+        }
+
+
+    }
+}
