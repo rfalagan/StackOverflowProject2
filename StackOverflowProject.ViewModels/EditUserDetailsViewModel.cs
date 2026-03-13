c75766c93e780d72d99a3dac98fcf6a2e904dfa9
Rafael Falagan <rfalagan@yahoo.com>
2026-03-13 08:16:12 -0400

    Commit 03/13/2026

--- StackOverflowProject.ViewModels/EditUserDetailsViewModel.cs
+++ StackOverflowProject.ViewModels/EditUserDetailsViewModel.cs
@@ -10,11 +10,14 @@ namespace StackOverflowProject.ViewModels
     public class EditUserDetailsViewModel
     {
 
+
+        public int UserID { get; set; }
+
         [Required]
         [RegularExpression(@"(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})")]
         public string Email { get; set; }
         [Required]
-        [RegularExpression(@"^[a-zA-Z]*$")]
+        [RegularExpression(@"^[a-zA-Z ]*$")]
         public string Name { get; set; }
         [Required]
         public string Mobile { get; set; }
